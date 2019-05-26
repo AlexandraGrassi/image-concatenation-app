1725ac60aff600275f29ad4727e7fa70110255ce
sasha.eroe <sasha.eroe@gmail.com>
2019-05-26 23:25:08 +0300

    Add responsive canvas

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 const canvas = document.getElementById("photosContainer");
 const ctx = canvas.getContext("2d");
-
 const inputs = Array.from(document.getElementsByTagName("input"));
 const images = [];
+let pixelRatio;
 
-inputs.forEach( (input, index) => {
-    if(input.type === "file") {
+document.addEventListener('DOMContentLoaded',() => {
+    canvas.width = 0;
+    /*pixelRatio = getPixelRatio();
+    ctx.scale(pixelRatio, pixelRatio);
+    canvas.style.width = canvas.width * pixelRatio + 'px';
+    canvas.style.height = canvas.height * pixelRatio + 'px';*/
+});
+
+inputs.forEach((input, index) => {
+    if (input.type === "file") {
         input.onchange = (e) => {
             let files = e.target.files;
             let curImg = new Image();
             images[index] = curImg;
-            pushToCanvas(files, curImg, index);
-            console.log(images);
+            loadImage(files, curImg, index);
         };
     }
 });
 
-const getAspectRation = (width, height) => width / height;
+/*const getPixelRatio = () => {
+    let factor = 1;
+
+    // retina display?
+    const isRetina = (window.devicePixelRatio > 1);
+    // iOS? (-> no auto double)
+    const isIOS = ((ctx.webkitBackingStorePixelRatio < 2) || (ctx.webkitBackingStorePixelRatio === undefined));
+
+    if (isRetina && isIOS) {
+        factor = 2;
+    }
+    return factor;
+};*/
+
+const getAspectRatio = (width, height) => width / height;
 
 const getOffset = (index, canvasWidth) => {
     let offset = 0;
-    for(let i = 1; i <= index; i++) {
-        if(images[i-1]) {
-            offset = offset + images[i-1].width;
+    for (let i = 1; i <= index; i++) {
+        if (images[i - 1]) {
+            offset = offset + images[i - 1].width;
         } else {
             offset = canvasWidth - images[i].width;
         }
@@ -30,28 +51,41 @@ const getOffset = (index, canvasWidth) => {
     return offset;
 };
 
-function loadImage(img, index) {
+function pushToCanvas(img, index) {
     img.onload = () => {
-        const { width: imgWidth, height: imgHeight } = img;
-        const { width: canvasWidth, height: canvasHeight } = canvas;
+        const {width: imgWidth, height: imgHeight} = img;
+        const {width: canvasWidth, height: canvasHeight} = canvas;
 
-        const aspectRatio = getAspectRation(imgWidth, imgHeight);
-        img.width = canvasHeight * aspectRatio;
+        const imgAspectRatio = getAspectRatio(imgWidth, imgHeight);
+
         img.height = canvasHeight;
+        img.width = canvasHeight * imgAspectRatio;
 
         const imgOffset = getOffset(index, canvasWidth);
+        if( (canvasWidth - imgOffset) < img.width && canvasWidth !== 0) {
+            canvas.width = canvasWidth + imgOffset - (canvasWidth - imgOffset);
+        }
+
+        if(canvasWidth === 0) {
+            canvas.width = img.width;
+            canvas.style.border = '2px solid gold';
+        }
         ctx.drawImage(img, imgOffset, 0, img.width, img.height);
     };
 }
 
-function pushToCanvas(files, img, index) {
+function loadImage(files, img, index) {
     if (FileReader && files && files.length) {
         const fr = new FileReader();
         fr.readAsDataURL(files[0]);
 
         fr.onload = () => {
             img.src = fr.result;
-            loadImage(img, index);
+            pushToCanvas(img, index);
+            console.log(images);
+
         };
+
     }
 }
+
